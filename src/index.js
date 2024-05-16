c52fabbef84dbbbd2257e56f2391f31d9c9f418b
OranMizrahi <54274776+OranMizrahi@users.noreply.github.com>
2024-05-16 15:21:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+const token = "ch3jdi34er"
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
